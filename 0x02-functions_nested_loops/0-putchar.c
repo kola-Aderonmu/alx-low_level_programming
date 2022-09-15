4ea8c5c184a47df746615479591cc7fc6bd00875
Adekola <gabkola94@gmail.com>
2022-09-15 11:37:11 -0700

    * main - print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,37 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * main - Entry Point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	char word[9] = "Holberton";
-	int i;
+* main - print putchar
 
-	for (i = 0; i < 9; i++)
-		_putchar(word[i]);
+* Return: always 0
+
+*/
+
+
+
+int main(void)
+
+{
+
+	_putchar('_');
+
+	_putchar('p');
+
+	_putchar('u');
+
+	_putchar('t');
+
+	_putchar('c');
+
+	_putchar('h');
+
+	_putchar('a');
+
+	_putchar('r');
+
 	_putchar('\n');
 
+	
+
 	return (0);
+
 }
