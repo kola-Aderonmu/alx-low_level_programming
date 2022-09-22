9fe5eaff2b70cce82bb804046a692258a0a95bd9
Adekola <gabkola94@gmail.com>
2022-09-22 09:23:43 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,24 +1,21 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-*0-strcat-concatenates the spring pointed to by @src to
-*the end of the string pointed to by @dest
-*@dest: String that will be appended
-*@src: String to be concantenated upon
-*
-*Return: returns poiner to @dest
-*/
-
-char *_strcat(char *dest,char *src)
+ * _strcat - a function that concatenates two strings.
+ * @dest: an input string
+ * @src: an input string
+ * Return: A pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
 {
+	char *temp = dest;
 
-int index = 0, dest_len = 0;
+	while (*dest)
+		dest++;
 
-        while (dest[index++])
-                dest_len++;
+	while (*src)
+		*dest++ = *src++;
 
-        for (index = 0; src[index]; index++)
-                dest[dest_len++] = src[index];
-
-        return (dest);
+	*dest = '\0';
+	return (temp);
 }
